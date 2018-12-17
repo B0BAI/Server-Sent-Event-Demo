e7622f615caad2a5ee9c2525bd970dc10caa0689
B0BAI <git@bobaikato.io>
2018-12-17 18:37:36 +0000

    clean up now using lombok

--- src/main/java/io/bobaikato/demo/sse/Message.java
+++ src/main/java/io/bobaikato/demo/sse/Message.java
@@ -1,41 +1,14 @@
 package io.bobaikato.demo.sse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class Message {
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Message {
     private String from;
 
     private String message;
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Message(String from, String message) {
-
-        this.from = from;
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-
-        return "Message{" +
-                "from='" + from + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
