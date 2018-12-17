fb9b263d9e89f7928150b320fbf45b724d474d58
B0BAI <git@bobaikato.io>
2018-12-17 17:39:58 +0000

    Update ChatController.java

--- src/main/java/io/bobaikato/demo/sse/ChatController.java
+++ src/main/java/io/bobaikato/demo/sse/ChatController.java
@@ -1,5 +1,6 @@
 package io.bobaikato.demo.sse;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -20,9 +21,21 @@ public class ChatController {
         return customEmitterService.sendMessage(message);
     }
 
+    @GetMapping(path = "/loops")
+    public void sendMessages() {
+        for (int a = 0; a <= 100; ++a) {
+            customEmitterService.sendMessage(new Message("Bobai", "Hey " + a));
+        }
+    }
+
 
     @GetMapping(path = "/stream")
     public SseEmitter stream() {
+        return customEmitterService.stream();
+    }
+
+    @GetMapping(path = "/streams")
+    public SseEmitter streams() {
         return customEmitterService.stream();
     }
 }
