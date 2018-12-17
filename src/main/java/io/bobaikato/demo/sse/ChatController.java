3f5d5b0ec2fae78afca506f7dd442df61885995c
B0BAI <git@bobaikato.io>
2018-12-17 18:37:12 +0000

    Update ChatController.java

--- src/main/java/io/bobaikato/demo/sse/ChatController.java
+++ src/main/java/io/bobaikato/demo/sse/ChatController.java
@@ -1,6 +1,5 @@
 package io.bobaikato.demo.sse;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -9,33 +8,33 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 public class ChatController {
 
-    final private CustomEmitterService customEmitterService;
+    final private EmitterService emitterService;
 
     @Autowired
-    public ChatController(CustomEmitterService customEmitterService) {
-        this.customEmitterService = customEmitterService;
+    public ChatController(EmitterService emitterService) {
+        this.emitterService = emitterService;
     }
 
     @PostMapping(path = "/chat", consumes = "application/json", produces = "application/json")
     public Message sendMessage(@RequestBody Message message) {
-        return customEmitterService.sendMessage(message);
+        return emitterService.sendMessage(message);
     }
 
     @GetMapping(path = "/loops")
     public void sendMessages() {
         for (int a = 0; a <= 100; ++a) {
-            customEmitterService.sendMessage(new Message("Bobai", "Hey " + a));
+            emitterService.sendMessage(new Message("Bobai", "Hey " + a));
         }
     }
 
 
     @GetMapping(path = "/stream")
     public SseEmitter stream() {
-        return customEmitterService.stream();
+        return emitterService.stream();
     }
 
     @GetMapping(path = "/streams")
     public SseEmitter streams() {
-        return customEmitterService.stream();
+        return emitterService.stream();
     }
 }
