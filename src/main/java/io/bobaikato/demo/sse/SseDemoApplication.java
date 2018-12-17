e71d48f7f0aa079ae852cf03ff41da87039a56b5
B0BAI <git@bobaikato.io>
2018-12-17 21:47:03 +0000

    Update SseDemoApplication.java

--- src/main/java/io/bobaikato/demo/sse/SseDemoApplication.java
+++ src/main/java/io/bobaikato/demo/sse/SseDemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SseDemoApplication {
 
-    public static void main(String[] args) {
-
+    public static void main(String... args) {
         SpringApplication.run(SseDemoApplication.class, args);
     }
 }
