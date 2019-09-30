0de1f83f713631aaffafa13eee45019c25dab194
B0BAI <bobaikato@gmail.com>
2019-09-30 15:05:53 +0100

    implement Serializable

--- src/main/java/io/b0bai/demo/sse/Message.java
+++ src/main/java/io/b0bai/demo/sse/Message.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-class Message {
+class Message implements Serializable {
+    private static final long serialVersionUID = 1428935322269489609L;
     private String from;
     private String message;
 }
